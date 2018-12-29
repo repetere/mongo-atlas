5b27c2f4b04c0928b03d0031c2585d8d3ef32ff4
Jan Bialostok <janbialostok@gmail.com>
2018-12-29 09:09:43 -0500

    add base class, add factories for prototype method initialization

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const {
+  request,
+  factories,
+} = require('./lib');
+
+class MongoAtlas {
+  constructor(user, apikey) {
+    this.user = user;
+    this.apikey = apikey;
+  }
+  async request(url, method, body, headers) {
+    return await request(url, {
+      method,
+      body: (body) ? JSON.stringify(body) : undefined,
+      headers: Object.assign({
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${Buffer.from(this.user + ':' + this.apikey).toString('base64')}`,
+      }, headers),
+    });
+  }
+}
+
+Object.keys(factories).forEach(method => {
+  MongoAtlas.prototype[method] = factories[method]();
+});
