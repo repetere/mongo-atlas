eaddcf2282bf3b78b56aff888168ec5fb34a1ca0
Jan Bialostok <janbialostok@gmail.com>
2018-12-31 09:48:03 -0500

    remove console log

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ class MongoAtlas {
     this.apikey = apikey;
   }
   async request(url, method, body, headers) {
-    console.log(url, {
-      method,
-      body: (body) ? JSON.stringify(body) : undefined,
-      headers: Object.assign({
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${Buffer.from(this.user + ':' + this.apikey).toString('base64')}`,
-      }, headers),
-    }, this.user, this.apikey);
     return await request(url, {
       method,
       body: (body) ? JSON.stringify(body) : undefined,
