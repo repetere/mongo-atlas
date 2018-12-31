55238565e1cfe9b8338da2f8a63ca8660fa3f39d
Jan Bialostok <janbialostok@gmail.com>
2018-12-31 09:45:39 -0500

    add support for users and project ip whitelisting

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ class MongoAtlas {
     this.apikey = apikey;
   }
   async request(url, method, body, headers) {
+    console.log(url, {
+      method,
+      body: (body) ? JSON.stringify(body) : undefined,
+      headers: Object.assign({
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${Buffer.from(this.user + ':' + this.apikey).toString('base64')}`,
+      }, headers),
+    }, this.user, this.apikey);
     return await request(url, {
       method,
       body: (body) ? JSON.stringify(body) : undefined,
@@ -24,3 +32,5 @@ class MongoAtlas {
 Object.keys(factories).forEach(method => {
   MongoAtlas.prototype[method] = factories[method]();
 });
+
+module.exports = MongoAtlas;
